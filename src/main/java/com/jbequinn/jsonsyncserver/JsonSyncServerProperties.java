6954ac207cea6c5beed6e9c71bb0b210dcf7636b
Juan Manuel <jbequinn@gmail.com>
2020-02-09 20:23:16 +0100

    [jsonsyncserver] update spring to 2.3.0 M1

--- src/main/java/com/jbequinn/jsonsyncserver/JsonSyncServerProperties.java
+++ src/main/java/com/jbequinn/jsonsyncserver/JsonSyncServerProperties.java
@@ -1,7 +1,6 @@
 package com.jbequinn.jsonsyncserver;
 
 import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotBlank;
